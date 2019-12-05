1fa8308e215619f0bd924e97086b273e2108c1d8
James Grider <james.grider@faa.gov>
2019-12-05 15:34:52 -0600

    Add minor color configurability to textbox

--- pygeng/inputArea.py
+++ pygeng/inputArea.py
@@ -1,4 +1,5 @@
-from PySide2.QtWidgets import QWidget, QLabel, QHBoxLayout
+from PySide2.QtGui import QPalette, QColor
+from PySide2.QtWidgets import QWidget, QLabel, QHBoxLayout, QLineEdit
 
 class InputArea(QWidget):
   def __init__(self, config):
@@ -11,7 +12,18 @@ class InputArea(QWidget):
     promptText = inputAreaConfig.get('prompt_text', 'pygeng')
     prompt = QLabel(promptText)
 
+    # Define the textbox
+    userInputConfig = inputAreaConfig.get('user_input', {})
+    userInputColor = QColor()
+    userInputColor.setNamedColor(userInputConfig.get('background', '#00000000'))
+    userInputPalette = QPalette()
+    userInputPalette.setColor(QPalette.Base, userInputColor)
+    userInput = QLineEdit()
+    userInput.setPalette(userInputPalette)
+    userInput.setFrame(False)
+
     # Layout
     layout = QHBoxLayout()
     layout.addWidget(prompt)
+    layout.addWidget(userInput)
     self.setLayout(layout)
