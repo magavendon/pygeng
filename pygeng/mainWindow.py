6a94be97ab304474a4cb1ab27dee1f5b250e9581
James Grider <james.grider@faa.gov>
2019-12-06 15:26:54 -0600

    Begin using config solely for window creation

--- pygeng/mainWindow.py
+++ pygeng/mainWindow.py
@@ -1,18 +1,108 @@
+from PySide2.QtGui import QPalette, QColor
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QWidget, QVBoxLayout
+from PySide2.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, \
+QLineEdit
 
-from inputArea import InputArea
+def setupContainer(objectInfo):
+  widget = QWidget()
+
+  # Define the looks for this widget and sub-widgets.
+  widgetLooks = objectInfo.get('looks', {})
+  if len(widgetLooks) > 0:
+    # If the background color was given,
+    if 'background' in widgetLooks:
+      # Define the color.
+      w_color = QColor()
+      w_color.setNamedColor(widgetLooks['background'])
+
+      # Set the background to that color.
+      w_background = QPalette()
+      w_background.setColor(QPalette.Base, w_color)
+      widget.setPalette(w_background)
+
+  # Define the layout for this widget.
+  w_layout = objectInfo.get('layout', 'horizontal')
+  layout = None
+  if w_layout == 'horizontal':
+    layout = QHBoxLayout()
+  elif w_layout == 'vertical':
+    layout = QVBoxLayout()
+
+  # Add other widgets.
+  subObjects = objectInfo.get('contains', {})
+  for subName in subObjects:
+    subObject = subObjects[subName]
+    subObjectType = subObject.get('type', 'container')
+    if not 'looks' in subObject:
+      subObject['looks'] = widgetLooks
+    subWidget = guiTypes[subObjectType](subObject)
+    layout.addWidget(subWidget)
+
+  widget.setLayout(layout)
+  return widget
+
+def setupLabel(objectInfo):
+  widget = QLabel()
+
+  # Define the looks for this widget.
+  widgetLooks = objectInfo.get('looks', {})
+  if len(widgetLooks) > 0:
+    # If the background color was given,
+    if 'background' in widgetLooks:
+      # Define the color.
+      w_color = QColor()
+      w_color.setNamedColor(widgetLooks['background'])
+
+      # Set the background to that color.
+      w_background = QPalette()
+      w_background.setColor(QPalette.Base, w_color)
+      widget.setPalette(w_background)
+
+  # Define how this widget works.
+  w_info = objectInfo.get('info', {})
+  widget.setText(w_info.get('text', ''))
+  widget.setToolTip(w_info.get('onhover', ''))
+
+  return widget
+
+def setupLineEdit(objectInfo):
+  widget = QLineEdit()
+
+  # Define the looks for this widget.
+  widgetLooks = objectInfo.get('looks', {})
+  if len(widgetLooks) > 0:
+    # If the background color was given,
+    if 'background' in widgetLooks:
+      # Define the color.
+      w_color = QColor()
+      w_color.setNamedColor(widgetLooks['background'])
+
+      # Set the background to that color.
+      w_background = QPalette()
+      w_background.setColor(QPalette.Base, w_color)
+      widget.setPalette(w_background)
+
+  return widget
+
+guiTypes = {
+    'container' : setupContainer,
+    'label'     : setupLabel,
+    'line_edit' : setupLineEdit,
+}
 
 class MainWindow(QWidget):
   def __init__(self, config):
     QWidget.__init__(self)
     self.setWindowFlags(Qt.FramelessWindowHint | Qt.Dialog)
-    self.config = config
 
-    # Create the window
-    inputArea = InputArea(self.config)
+    # Add defined sections from config file
+    if len(config) > 0:
+      layout = QVBoxLayout()
 
-    # Layout
-    layout = QVBoxLayout()
-    layout.addWidget(inputArea)
-    self.setLayout(layout)
+      for guiName in config:
+        guiObject = config[guiName]
+        guiObjectType = guiObject.get('type', 'container')
+        guiWidget = guiTypes[guiObjectType](guiObject)
+        layout.addWidget(guiWidget)
+
+      self.setLayout(layout)
--- pygeng/pygeng.py
+++ pygeng/pygeng.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from pathlib import Path
 import sys
 import yaml
@@ -7,6 +8,17 @@ from PySide2.QtWidgets import QApplication
 from mainWindow import MainWindow
 
 def loadConfigFile():
+  def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
   try:
     configPath = '{}/.config/pygeng'.format(str(Path.home()))
     configFile = open('{}/config'.format(configPath))
@@ -14,13 +26,14 @@ def loadConfigFile():
     return {}
 
   try:
-    config = yaml.load(configFile, Loader = yaml.FullLoader)
+    config = ordered_load(configFile, yaml.SafeLoader)
   except yaml.parser.ParserError as e:
     logFilename = '{}/.pygeng.log'.format(str(Path.home()))
     logFile = open(logFilename, 'a')
     logFile.write('Config file has bad formatting [{}]'.format(
       str(e).split(', ')[2]))
 
+  print(config)
   return config
 
 def main():
